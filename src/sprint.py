8ad8b6547c297765186c86142a48932c9915b2b9
bokney <toknyk@gmail.com>
2024-06-22 23:01:46 +0100

    initial commit for sprint, clear and wait

---
+++ src/sprint.py
@@ -0,0 +1,15 @@
+
+import os, time, sys
+ 
+def sprint(*args):
+    string = "".join(map(str,args))
+    for char in string + '\n':
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(4./120)
+
+def clear():
+    os.system('cls' if os.name == 'nt' else "printf '\033c'")
+
+def wait(length):
+    time.sleep(length)
